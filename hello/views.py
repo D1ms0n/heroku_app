1fe56d91d91ee5b4c2db1730bbe063378ffdfea6
D1ms0n <dmytro.morozov@ukr.net>
2017-03-10 21:02:38 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1120, 550)
+    driver.get("http://studio7day.herokuapp.com/")
+    
+    a = driver.find_elements_by_css_selector('button')
+    b = len(a)
+    
+    
+    
+    return render(request, 'index.html', {'b': b})
 
 
 def db(request):
